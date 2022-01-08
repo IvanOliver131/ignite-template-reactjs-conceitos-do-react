0eceac5ce69d45a34b4afdbcb81ac50b93c889d5
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-01-08 17:21:36 -0300

    :tada: Challenge finallize

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -14,16 +14,62 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  useEffect(() => {
+    setTasks(tasks);
+  }, [tasks]);
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return alert('Você precisa informar algum titulo');
+    } 
+    const newTask: Task = {
+      id: Math.floor(Date.now() * Math.random()),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    // Pego o estado antigo das tasks e adiciono o newTask no final
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    // ######### FEITO POR MIM - INICIO #########
+    // const teste = tasks.map((task) => {
+    //   if (task.id == id) {
+    //     if (task.isComplete == true) {
+    //       task.isComplete = false;
+    //     } else {
+    //       task.isComplete = true;
+    //     }        
+    //   }
+
+    //   return task
+    // });
+
+    // setTasks(teste);
+
+    // ######### FEITO POR MIM - FIM #########
+
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task, 
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
+
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    // O filter remove a task que não cumpre a condição passada
+    const filteredTasks = tasks.filter(task => task.id !== id);
+  
+    setTasks(filteredTasks);
   }
 
   return (
@@ -32,7 +78,7 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
+          <input
             type="text" 
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
